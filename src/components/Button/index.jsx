3c15df512c12e8726bb170947f080051cb6a8102
Icaro <luizicaro.s@gmail.com>
2023-04-06 18:37:53 -0300

    Implement Button component

---
+++ src/components/Button/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Button.module.css';
+
+export default function Button({ children, size }) {
+  return (
+    <button 
+      className={`${styles.button} ${styles[size]}`}
+    >
+      {children}
+    </button>
+  )
+}
