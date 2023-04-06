8da2515a0bd51478f9bae1ec2698f01c26e09c15
Icaro <luizicaro.s@gmail.com>
2023-04-06 11:46:39 -0300

    Adding Routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import AboutMe from "./pages/AboutMe";
+import Home from "./pages/Home";
+
 function App() {
   return (
-    <h1>Hello Word!</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/aboutMe" element={<AboutMe />} />
+        <Route path="*" element={<div>Página não encontrada</div>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
