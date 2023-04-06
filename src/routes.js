f5c02ecf82b914d797faa456f47fee09c2504ac6
Icaro <luizicaro.s@gmail.com>
2023-04-06 17:39:01 -0300

    Add post route

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,5 @@
 import Footer from "components/Footer";
+import Post from "components/Post";
 import DefaultPage from "pages/DefaultPage";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
@@ -14,6 +15,7 @@ function AppRoutes() {
         <Route path="/" element={<DefaultPage />}>
           <Route index element={<HomePage />} />
           <Route path="aboutMe" element={<AboutMe />} />
+          <Route path="post/:id" element={<Post />} />
         </Route>
 
         <Route path="*" element={<div>Página não encontrada</div>} />
