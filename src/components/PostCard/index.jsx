c33aea832012bb635579e89b91355962daee355d
Icaro <luizicaro.s@gmail.com>
2023-04-06 17:41:04 -0300

    Add react-router-dom with Link tag

--- src/components/PostCard/index.jsx
+++ src/components/PostCard/index.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import styles from './PostCard.module.css';
 
 export default function PostCard({ post }) {
   return (
-    <div className={styles.post}>
-      <img 
-        className={styles.cover}
-        src={`/assets/posts/${post.id}/cover.png`}
-        alt='Imagem de capa'
-      />
+    <Link to={`/post/${post.id}`}>
+      <div className={styles.post}>
+        <img 
+          className={styles.cover}
+          src={`/assets/posts/${post.id}/cover.png`}
+          alt='Imagem de capa'
+        />
 
-      <h2 className={styles.title}>{post.title}</h2>
+        <h2 className={styles.title}>{post.title}</h2>
 
-      <button className={styles.readButton}>Ler</button>
-    </div>
+        <button className={styles.readButton}>Ler</button>
+      </div>
+    </Link>
   )
 }
