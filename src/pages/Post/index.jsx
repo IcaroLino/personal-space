2ade9d467373f466516f4335d699c0de0a788f37
Icaro <luizicaro.s@gmail.com>
2023-04-14 18:20:58 -0300

    Implement recommended post to Post page

--- src/pages/Post/index.jsx
+++ src/pages/Post/index.jsx
@@ -4,6 +4,8 @@ import posts from 'json/posts.json';
 import './Post.css';
 import { useParams } from 'react-router-dom';
 import { ReactMarkdown } from 'react-markdown/lib/react-markdown';
+import styles from './Post.module.css';
+import PostCard from 'components/PostCard';
 
 export default function Post() {
 
@@ -13,10 +15,18 @@ export default function Post() {
 
   if (!post) return <h1>Post não encontrado!</h1>
 
+  const recommendedPost = posts
+      .filter((post) => {
+        if (param.id > 4) return post.id < Number(param.id)
+        return post.id !== Number(param.id)
+      })
+      .sort((a,b) => b.id - a.id)
+      .slice(0, 4);
+
   return (
     <PostContent
       coverPicture={`/assets/posts/${post.id}/cover.png`}
-       title={post.title}
+      title={post.title}
     >
 
       <div className='post-markdown-container'>
@@ -24,6 +34,18 @@ export default function Post() {
           {post.text}
         </ReactMarkdown>
       </div>
+
+      <h2 className={styles.othersPostsTitle}>
+        Outros posts que você pode gostar:
+      </h2>
+
+      <ul className={styles.recommendedPosts}>
+        {recommendedPost.map((post) => (
+          <li key={post.id}>
+            <PostCard post={post} />
+          </li>
+        ))}
+      </ul>
     
     </PostContent>
   )
