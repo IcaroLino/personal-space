5698177d3fed4260148f94c4ff1ba621fe177616
Icaro <luizicaro.s@gmail.com>
2023-04-14 17:53:10 -0300

    Implement ScrollToTop component

--- src/routes.js
+++ src/routes.js
@@ -6,17 +6,19 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import AboutMe from "./pages/AboutMe";
 import HomePage from "./pages/HomePage";
+import ScrollToTop from "components/ScrollToTop";
 
 function AppRoutes() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Header />
 
       <Routes>
         <Route path="/" element={<DefaultPage />}>
           <Route index element={<HomePage />} />
           <Route path="aboutMe" element={<AboutMe />} />
-          <Route path="post/:id" element={<Post />} />
+          <Route path="post/:id/*" element={<Post />} />
         </Route>
 
         <Route path="*" element={<NotFound />} />
